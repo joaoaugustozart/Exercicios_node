da2c249e706551007911cbc9276b8da1c36811bf
João Zart <84188065+joaoaugustozart@users.noreply.github.com>
2024-03-08 14:57:00 -0300

    Add files via upload

---
+++ print e matematica.js
@@ -0,0 +1,18 @@
+// comando print
+console.log("O CAXIAS VAI GANHAR O GAUCHÃO")
+
+// comando de variaveis constantes
+
+const num1 = 20;
+const num2 = 654;
+
+//soma
+let soma = num1 + num2;
+let soma2 = num2 - num1;
+let soma3 = num2 / num1;
+let soma4 = num1 * num2;
+
+console.log("O valor da soma é " + soma);
+console.log("O valor da subtração é " + soma2);
+console.log("O valor da divisão é " + soma3);
+console.log("O valor da multiplicação é " + soma4);
